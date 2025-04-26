7d8b1ff27155ff96349bac2055943a45b562acfa
vivek08wrk <vivekrawat08wrk@gmail.com>
2025-04-26 14:04:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ['firebase/firestore'], // Externalize Firebase
-    },
-  },
-});
+  plugins: [ tailwindcss(),],
+})
