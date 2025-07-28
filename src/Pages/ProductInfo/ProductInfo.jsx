953bfda7a14cc65455e07a7dc66548b2521d16b6
vivek08wrk <vivekrawat08wrk@gmail.com>
2025-07-28 11:58:33 +0530

    Update ProductInfo.jsx

--- src/Pages/ProductInfo/ProductInfo.jsx
+++ src/Pages/ProductInfo/ProductInfo.jsx
@@ -59,11 +59,12 @@ function ProductInfo() {
         <Layout>
             <section className="text-gray-600 body-font overflow-hidden">
                 <div className="container px-5 py-10 mx-auto">
-                    {products && 
+                    {products &&
                     <div className="lg:w-4/5 mx-auto flex flex-wrap">
                         <img
                             alt="ecommerce"
-                            className="lg:w-1/3 w-full lg:h-auto  object-cover object-center rounded"
+                            // CHANGED: lg:w-1/3 to lg:w-1/2, object-cover to object-contain
+                            className="lg:w-1/2 w-full lg:h-auto object-contain object-center rounded"
                             src={products.imageUrl}
                         />
                         <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
