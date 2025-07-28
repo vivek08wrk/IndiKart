cf55e7ed34168889316e23ea31cc996a3dde09b7
vivek08wrk <vivekrawat08wrk@gamil.com>
2025-07-28 12:45:10 +0530

    Updated HeroSection component

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>
-    <img src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="" />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 0, padding: 0 }}>
+    <img src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="" style={{ width: '100%', display: 'block' }} />
     </div>
   )
 }
